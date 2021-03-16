dd7360f9f44014a7ac12c92cef8c0d4f4519b92a
xanqus <sbk98031816@gmail.com>
2021-03-16 18:36:24 +0900

    finish

--- src/containers/TodosContainer.jsx
+++ src/containers/TodosContainer.jsx
@@ -1,55 +1,30 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { changeInput, insert, toggle, remove } from '../modules/todos';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import todos, { changeInput, insert, toggle, remove } from '../modules/todos';
 import { increase } from '../modules/counter';
 import Todos from '../components/Todos';
+import useActions from '../lib/useActions';
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-  number,
-  increase,
-}) => {
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
+  const [onChangeInput, onInsert, onToggle, onRemove, onIncrease] = useActions(
+    [changeInput, insert, toggle, remove, increase],
+    [],
+  );
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
-      onIncrease={increase}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
+      onIncrease={onIncrease}
     ></Todos>
   );
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  changeInput: (input) => {
-    dispatch(changeInput(input));
-  },
-  insert: (text) => {
-    dispatch(insert(text));
-  },
-  toggle: (id) => {
-    dispatch(toggle(id));
-  },
-  remove: (id) => {
-    dispatch(remove(id));
-  },
-  increase: () => {
-    dispatch(increase());
-  },
-});
-
-export default connect(
-  ({ todos, counter }) => ({
-    input: todos.input,
-    todos: todos.todos,
-    number: counter.number,
-  }),
-  mapDispatchToProps,
-)(TodosContainer);
+export default React.memo(TodosContainer);
--- src/modules/counter.jsx
+++ src/modules/counter.jsx
@@ -1,3 +1,5 @@
+import { createAction, handleActions } from 'redux-actions';
+
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
@@ -8,19 +10,12 @@ const initialState = {
   number: 0,
 };
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
 
 export default counter;
